c124d4605d559dbf21d8c0288c851b1612122f00
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-06-24 19:24:48 -0300

    🩹 Adiciona metodos type hinting faltantes

--- models/utils.pyi
+++ models/utils.pyi
@@ -8,5 +8,9 @@ class BaseModel(exts.db.Model):
     query: orm.Query
     session: orm.Session
 
+    def save(self, commit: bool=False)  -> None: ...
+
+    def delete(self, commit :bool=False) -> None: ...
+
     def serialize(self, *, ignore: Callback=None, mapdict: Callback=None,
                   dict_factory=dict) -> dict: ...
